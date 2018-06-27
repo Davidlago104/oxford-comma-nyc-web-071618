7422a056e2e90758784748c4d69de9d7ea8ee1db
Davidlago104 <davidlago104@gmail.com>
2018-06-27 23:15:34 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+    if array.length == 1
+       array[0]
+    else if array.length == 2
+       array.join(" and ")
+     else if array.length == 3 
+       array.
+     end
+     end
+    end
 end
