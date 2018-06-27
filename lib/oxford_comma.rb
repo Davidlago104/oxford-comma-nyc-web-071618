be544b3de865db62856908d4c5d1349c2347e58a
Davidlago104 <davidlago104@gmail.com>
2018-06-27 23:24:35 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(array)
        array.join(" and ")
      else if array.length == 3 
        array.insert(2, "and").map do |word|
-         if word !== "and"
+         if word != "and"
            word + ","
          else
            word
