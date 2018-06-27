a3efa064771832c3e3d7835b47a8f24c04d99e2f
Davidlago104 <davidlago104@gmail.com>
2018-06-27 23:20:08 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
     else if array.length == 2
        array.join(" and ")
      else if array.length == 3 
-       array[2].join(" and ")
+       array.insert()
      end
      end
     end
